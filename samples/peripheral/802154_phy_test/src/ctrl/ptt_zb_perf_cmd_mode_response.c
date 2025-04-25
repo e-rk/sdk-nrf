60050ea3d6c32389715c4f4b70981064157b1408
Rafał Kuźnia <rafal.kuznia@nordicsemi.no>
2025-04-25 13:31:32 +0200

    print

--- samples/peripheral/802154_phy_test/src/ctrl/ptt_zb_perf_cmd_mode_response.c
+++ samples/peripheral/802154_phy_test/src/ctrl/ptt_zb_perf_cmd_mode_response.c
@@ -473,3 +473,11 @@ void cmd_uart_send_rsp_l_get_gpio_error(ptt_evt_id_t evt_id)
 
 	ptt_uart_send_packet(evt_data->arr, strlen(PTT_CAST_TO_STR(evt_data->arr)));
 }
+
+void cmd_uart_send_rsp_l_tx_finished(uint8_t frames, uint8_t acks)
+{
+	char buf[40];
+	sprintf(buf, "0x000000%02x 0x000000%02x", frames, acks);
+
+	ptt_uart_send_packet(buf, strlen(buf));
+}
